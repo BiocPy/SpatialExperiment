108ba1fcdc0cfa26ab23c2fa34142a3918c3bb0d
keviny2 <kevinyang10@gmail.com>
2025-02-21 15:40:27 -0800

    Add test for duplicate sample ids for combine ops

---
+++ tests/test_spe_combine.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+
+import pytest
+import numpy as np
+import biocutils as ut
+from spatialexperiment import SpatialExperiment
+
+__author__ = "keviny2"
+__copyright__ = "keviny2"
+__license__ = "MIT"
+
+
+def test_duplicate_sample_ids(spe):
+    with pytest.warns(UserWarning):
+        combined = ut.combine_columns(spe, spe)
+
+    assert len(combined.column_data["sample_id"].unique()) == 2 * len(
+        spe.column_data["sample_id"].unique()
+    )
+    assert combined.shape[0] == spe.shape[0]
+    assert combined.shape[1] == 2 * spe.shape[1]
+    assert combined.rownames == spe.rownames
+    assert set(combined.colnames.as_list()) == set(spe.colnames.as_list())
